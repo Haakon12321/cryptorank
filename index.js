6d694c6662ced9854f81b0b0a77b9f6dae1794d6
0xHakon <101798843+Haakon12321@users.noreply.github.com>
2026-03-19 09:09:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const WEBHOOK = "PASTE_YOUR_WEBHOOK_URL";
+const WEBHOOK = "https://discord.com/api/webhooks/1484097341102227529/UDIhySx9KMIE5IkTSJKFeFwQ_qD2g-Jyn_r9cThSDZrK-mopyPJidDTWZXJAga75Z-X3";
 
 async function send() {
   await axios.post(WEBHOOK, {
